1e953ab50c08e965a597e2cb864187f00762d3a3
Alexei Skorik <sko-design@yandex.ru>
2024-07-28 12:48:17 +0300

    Корректировки от ревьювера

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Фотография горы с высоты птичьего полета",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Фотография реки между гор",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Фотография поанельных домов",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Фотография земли в горах",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Фотография железнодорожных рельс посреди леса с высоты",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Фотография обрыва на горе зимой",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,36 @@
 // @todo: DOM узлы
-const placesList = document.querySelector(".places__list");
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+const cardImage = '.card__image';
+const catdTitle = '.card__title';
+const cardDeleteButton = '.card__delete-button';
+const placeItem = '.places__item';
 
 // @todo: Функция создания карточки
-function addCard(imageValue, titleValue) {
-    // @todo: Темплейт карточки
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  
-    cardElement.querySelector('.card__image').src = imageValue;
-    cardElement.querySelector('.card__title').textContent = titleValue;
-    // @todo: Функция лакйа карточки
-    cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
-      evt.target.classList.toggle('card__like-button_is-active');
-    });
-    // @todo: Функция удаления карточки
-    cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
-        evt.target.parentNode.remove();
-    });
-  
-  placesList.append(cardElement)
+function createCard(data) {
+  const { link, alt, name } = data;
+  const cardElement = cardTemplate.querySelector(placeItem).cloneNode(true);
+
+  cardElement.querySelector(cardImage).src = link;
+  cardElement.querySelector(cardImage).alt = alt;
+  cardElement.querySelector(catdTitle).textContent = name;
+
+  return cardElement;
+}
+
+// @todo: Функция добавления карточки
+function appendCard(cardElement) {
+  cardElement.querySelector(cardDeleteButton).addEventListener('click', deleteCard);
+
+  placesList.append(cardElement);
+}
+
+// @todo: Функция удаления карточки
+function deleteCard(evt) {
+  evt.target.closest(placeItem).remove();
 }
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(card => {
-    addCard(card.link, card.name);
+  appendCard(createCard(card));
 });
